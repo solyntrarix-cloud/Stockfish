91c63fdd3fbe9f785f1435c6f7a3e301acd54c73
solyntrarix-cloud <solyntrarix@gmail.com>
2026-02-04 22:12:17 +0000

    Tune DRFP-CH safety margin (divisor / 4)

--- src/search.cpp
+++ src/search.cpp
@@ -882,7 +882,7 @@ Value Search::Worker::search(
 
             return futilityMult * d
                  - (2474 * improving + 331 * opponentWorsening) * futilityMult / 1024  //
-                 + std::abs(correctionValue) / 174665;
+                 + std::abs(correctionValue) / 43666;
         };
 
         if (!ss->ttPv && depth < 14 && eval - futility_margin(depth) >= beta && eval >= beta
